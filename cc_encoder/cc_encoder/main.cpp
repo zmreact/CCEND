4e37bf36246d338b21dba1fc8e82d2bc6d72d7e5
Denis <zmreact@gmail.com>
2017-09-28 20:37:44 +0300

    cc_encoder. pre-v1.0. Кодер написан. Предрелизная версия. В релиз пустим, когда будет написан генератор шума и декодер и проверено их взаимодействие.

--- cc_encoder/cc_encoder/main.cpp
+++ cc_encoder/cc_encoder/main.cpp
@@ -34,34 +34,41 @@ int main(int argc, const char * argv[]) {
                 case E:
                 {
                     input input;
-                    const int * s_data = input.s_data;
-                    const int * s_data_size = input.s_data_size;
-                    const int * s_sequience = input.s_sequience;
-                    const int * f_polynomial = input.f_polynomial;
+                    const int * s_data = nullptr;
+                    const int * const s_data_size = input.s_data_size;
+                    const int * s_sequience = nullptr;
+                    const int * const f_polynomial = input.f_polynomial;
                     
-                    encoder encoder(s_data, s_data_size, f_polynomial);
-                    const int * aj_data = encoder.get_aj_data();
-                    const int aj_data_size = sizeof(*aj_data) * n;
-                    memcpy(shmem_data_pointer, aj_data, aj_data_size);
+                    encoder encoder(f_polynomial);
+                    
+                    //writing (7, 4) codes to shared memory
+                    s_data = input.s_data;
+                    s_sequience = input.s_sequience;
+                    for (int i = 0; i < *s_data_size; i++) {
+                        const int * s_data_i = new const int(s_data[*s_sequience - 1]);
+                        int * aj_data = new int(encoder.aj_data(s_data_i));
+                        int aj_data_size = sizeof(*aj_data);
+                        
+                        memcpy(shmem_data_pointer, aj_data, aj_data_size);
+                        s_sequience++;
+                        
+                        delete s_data_i, delete aj_data;
+                    }
                     
                     
                     cout << "E_STATE" << endl << endl;
                     
                     cout << "Source data codes: " << endl;
-                    for (int i = 0; i < *s_data_size; i++) {
-                        cout << bitset<k>(s_data[*s_sequience - 1]) << " ";
-                        s_sequience++;
-                    }
+                    s_data = input.s_data;
+                    s_sequience = input.s_sequience;
+                    for (int i = 0; i < *s_data_size; i++) cout << bitset<k>(s_data[*s_sequience++ - 1]) << " ";
                     cout << endl << endl;
                     
                     cout << "Encoding to (7, 4) anti-jamming cyclic codes..." << endl;
                     cout << "Forming polynomial: " << endl;
                     cout << bitset<k>(*f_polynomial) << endl;
                     cout << "Source anti-jamming codes:" << endl;
-                    
-                    for (int i = 0; i < *s_data_size; i++) {
-                        cout << bitset<n>(* (shmem_data_pointer + i)) << " ";
-                    }
+                    for (int i = 0; i < *s_data_size; i++) cout << bitset<n>(*(shmem_data_pointer + i)) << " ";
                     cout << endl << endl;
                     
                     cout << "Sending...";
