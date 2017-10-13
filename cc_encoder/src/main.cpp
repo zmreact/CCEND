a937b1947a5bd676404e2cc9a9bf75eba21b22c5
Denis <zmreact@gmail.com>
2017-10-13 19:33:48 +0300

    Улучшения для подготовки к кросс-платформенности. Улучшения читабельности кода.

--- cc_encoder/src/main.cpp
+++ cc_encoder/src/main.cpp
@@ -11,13 +11,12 @@ using namespace std;
 #define DEVELOPERS "Denis Petuhov, Kaledina Anastasija, Korotygin Aleksandr, Ezhova Elena, Krasnov Kirill"
 
 int main(int argc, const char * argv[]) {
-    cout << "University: " << UNIVERSITY << endl
-         << "Student group: " << STGROUP << endl
-         << "Developers: " << DEVELOPERS;
+    cout << "University: " << UNIVERSITY << endl << "Student group: " << STGROUP << endl << "Developers: " << DEVELOPERS;
     cout << endl << endl;
     
     shmem_alloc_and_clean();
     shmem.attach();
+    
     
     if (shmem.isAttached()) {
         cout << "Shared memory size: " << shmem.size() << " " << "Bytes";
@@ -30,32 +29,42 @@ int main(int argc, const char * argv[]) {
             switch (TURN) {
                 case E:
                     cout << "Source data codes: " << endl;
+                    
                     for (int i = 0; i < *input.s_data_size; i++) {
                         int * curr_dat = new int(input.s_data[input.s_sequience[i] - 1]);
                         cout << bitset<k>(*curr_dat) << " ";
                         delete curr_dat;
                     }
+                    
                     cout << endl << endl;
                     
+                    
                     cout << "Encoding to (7, 4) anti-jamming cyclic codes..." << endl;
+                    
                     cout << "Forming polynomial: " << endl;
+                    
                     cout << bitset<k>(f_polynomial) << endl;
+                    
                     cout << "Source anti-jamming codes:" << endl;
+                    
                     for (int i = 0; i < DATA_SIZE_INT; i++) {
                         int * curr_dat = new int(input.s_data[input.s_sequience[i] - 1]);
                         int * aj_data = new int(encoder.aj_data(curr_dat));
-
                         memcpy(DATA_PTR + i, aj_data, sizeof(*aj_data));
-
-                        delete aj_data, curr_dat;
+                        delete aj_data;
+                        delete curr_dat;
                     }
+                    
                     for (int i = 0; i < DATA_SIZE_INT; i++) cout << bitset<n>(*(DATA_PTR + i)) << " ";
                     cout << endl << endl;
+                    
                     
                     cout << "Sending...";
                     cout << endl << endl;
                     
+                    
                     WAIT_TURN;
+                    
                     
                     break;
                     
